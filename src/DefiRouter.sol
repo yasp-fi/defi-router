c429e08812f2da6dd02bbf780d4762525a7bedfb
Sai Kranthi <iamsaikranthi@gmail.com>
2023-05-13 21:27:01 +0530

    feat: list UniswapV2Module in supported modules

--- src/DefiRouter.sol
+++ src/DefiRouter.sol
@@ -7,12 +7,14 @@ import "./modules/compound/CompoundModule.sol";
 import "./modules/curve/CurveModule.sol";
 import "./modules/balancer/BalancerModule.sol";
 import "./modules/uniswapV3/UniswapV3Module.sol";
+import "./modules/uniswapV2/UniswapV2Module.sol";
 import "./utils/Multicall.sol";
 
 abstract contract SupportedModules is
   PeripheryPermit2,
   Multicall,
   AaveV3Module,
+  UniswapV2Module,
   UniswapV3Module,
   CompoundModule,
   BalancerModule
