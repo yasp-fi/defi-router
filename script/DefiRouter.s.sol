4e0a330a1282e1875affe49a59c96e32e525aa0a
George Molchanov <wertusser@gmail.com>
2023-05-14 01:44:07 +0200

    chore: small fixes

--- script/DefiRouter.s.sol
+++ script/DefiRouter.s.sol
@@ -5,15 +5,16 @@ import "forge-std/Script.sol";
 import "../src/DefiRouter.sol";
 
 contract DefiRouterScript is Script {
-  address PERMIT2 = address(0x000000000022D473030F116dDEE9F6B43aC78BA3);
+  address PERMIT2 = address(vm.envAddress("PERMIT2"));
   address WETH = address(vm.envAddress("WETH"));
 
   function run() public returns (DefiRouter deployed) {
-    uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
+    uint256 deployerPrivateKey = vm.deriveKey(vm.envString("MNEMONIC"), 0);
 
-    vm.startBroadcast(deployerPrivateKey);
     address broadcaster = vm.addr(deployerPrivateKey);
     console2.log("broadcaster", broadcaster);
+    
+    vm.startBroadcast(deployerPrivateKey);
 
     ImmutableState memory state =
       ImmutableState({ permit2: PERMIT2, weth9: WETH });
